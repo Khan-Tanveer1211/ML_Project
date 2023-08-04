636a9311902bd328c93364702845c07321a786c4
TanveerGit <tanveer.khancb@gmail.com>
2023-08-04 22:28:28 +0200

    app Flask

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler   # use it beacuse the use of pickle file
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 app = Flask(__name__)  # __name__ gives the enterpoint in Flask
 
@@ -18,4 +19,15 @@ def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
     else:
-        pass
+        data = CustomData(gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+)
+    
+    
+if __name__ =="__main__":
+    pass
